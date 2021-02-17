f103e675e7dcc2d174e71f26c2670a32029b5a4a
Urquhart <peyton.urquhart@wsu.edu>
2021-02-16 19:52:44 -0800

    Homescreen

---
+++ CPTS_487-Peyton-Connor-Diwashi/Windows/HomeScreen.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace CPTS_487_Peyton_Connor_Diwashi
+{
+    public partial class HomeScreen : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+        private Vector2 currentWindowResolution = new Vector2(1280, 720);
+
+        // List of all componenets for the GUI on the home screen
+        private List<GUIComponent> _gui = new List<GUIComponent>();
+
+        public HomeScreen()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        // The GUI is first initialized to fit a 720p resolution, everything can then be scaled to any resolution
+        protected override void Initialize()
+        {
+            // Play game button
+            GUIButton button_playgame = new GUIButton(new Vector2(20, 600), Content.Load<Texture2D>("Button1"), Content.Load<SpriteFont>("Font"), Color.Black, "Start");
+            button_playgame.Precidence = 100;
+            // Exit button
+            GUIButton button_exit = new GUIButton(new Vector2(340, 600), Content.Load<Texture2D>("Button1"), Content.Load<SpriteFont>("Font"), Color.Black, "Exit");
+            button_exit.Precidence = 100;
+            // Resolution Button
+            GUIButton button_res = new GUIButton(new Vector2(660, 600), Content.Load<Texture2D>("Button1"), Content.Load<SpriteFont>("Font"), Color.Black, "1920 x 1080");
+            button_res.Precidence = 100;
+            // Title screen
+            GUIButton title = new GUIButton(new Vector2(0, 0), Content.Load<Texture2D>("Homescreen"), Content.Load<SpriteFont>("Font"), Color.Black, null);
+            title.Precidence = 0;
+            title.ShadeOnHover = false;
+            // Call specific methods in HomeScreenEvent.cs when button events are Invoked
+            button_exit.Click += this.exitButton_Click;
+            button_playgame.Click += this.playButton_Click;
+            button_res.Click += this.resButton_Click;
+            // Add all GUIComponent types to a list of GUIComponents
+            _gui.Add(button_playgame);
+            _gui.Add(button_exit);
+            _gui.Add(button_res);
+            _gui.Add(title);
+            // Sort GUIComponents based off of GUIComponenet.Precidence to determine which should be drawn first (lower is first)
+            _gui.Sort((x, y) => x.Precidence.CompareTo(y.Precidence));
+            // Set preferred resolution for the home screen
+            _graphics.PreferredBackBufferHeight = (int)this.currentWindowResolution.Y;
+            _graphics.PreferredBackBufferWidth = (int)this.currentWindowResolution.X;
+            _graphics.ApplyChanges();
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            foreach(GUIComponent gc in _gui)
+            {
+                gc.Update(gameTime);
+            }
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.White);
+            _spriteBatch.Begin();
+
+            // Draw all GUI components
+            foreach (GUIComponent gc in _gui)
+            {
+                gc.Draw(gameTime, _spriteBatch);
+            }
+            _spriteBatch.End();
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ CPTS_487-Peyton-Connor-Diwashi/Windows/HomeScreenEvent.cs
@@ -0,0 +1,96 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+
+namespace CPTS_487_Peyton_Connor_Diwashi
+{
+    public partial class HomeScreen : Game
+    {
+        public event EventHandler e_exit;
+        public event EventHandler e_play;
+        public event EventHandler e_resolution;
+
+        /// <summary>
+        /// On exitButton Click event, invoke 'exit' event from the home window. (Program.cs subscribes, exit all windows)
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void exitButton_Click(object sender, System.EventArgs e)
+        {
+            e_exit.Invoke(this, new EventArgs());
+            Exit();
+        }
+
+        /// <summary>
+        /// On playButton Click event, invoke 'play' event from the home window.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void playButton_Click(object sender, System.EventArgs e)
+        {
+            e_play.Invoke(this, new EventArgs());
+        }
+
+        /// <summary>
+        /// On resButton Click event, scale GUI accordingly
+        /// Invokes e_resolution event, sender is a Vector2 of new current screen resolution
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void resButton_Click(object sender, System.EventArgs e)
+        {
+            GUIButton b = (GUIButton)sender;
+            if (b.Text == "1920 x 1080")
+            {
+                this.scaleGUI(1080.0f / 720.0f);
+                this.setBackBufferResolution(1080, 1920);
+                b.Text = "1024 x 576";
+                this.currentWindowResolution.X = 1920;
+                this.currentWindowResolution.Y = 1080;
+            }
+            else if (b.Text == "1280 x 720")
+            {
+                this.scaleGUI(720.0f / 576.0f);
+                this.setBackBufferResolution(720, 1280);
+                b.Text = "1920 x 1080";
+                this.currentWindowResolution.X = 1280;
+                this.currentWindowResolution.Y = 720;
+            }
+            else if (b.Text == "1024 x 576")
+            {
+                this.scaleGUI(576.0f / 1080.0f);
+                this.setBackBufferResolution(576, 1024);
+                b.Text = "1280 x 720";
+                this.currentWindowResolution.X = 1024;
+                this.currentWindowResolution.Y = 576;
+            }
+            e_resolution.Invoke(this.currentWindowResolution, new EventArgs());
+        }
+
+        /// <summary>
+        /// Scale the entire GUI by a factor of n
+        /// </summary>
+        /// <param name="n"> scale factor </param>
+        private void scaleGUI(float n)
+        {
+            foreach (GUIComponent gc in this._gui)
+            {
+                gc.Scale(n);
+            }
+        }
+
+        /// <summary>
+        /// Changes the resolution of this window
+        /// </summary>
+        /// <param name="h"> height </param>
+        /// <param name="w"> width </param>
+        private void setBackBufferResolution(int h, int w)
+        {
+            this._graphics.PreferredBackBufferHeight = h;
+            this._graphics.PreferredBackBufferWidth = w;
+            this._graphics.ApplyChanges();
+        }
+    }
+}
